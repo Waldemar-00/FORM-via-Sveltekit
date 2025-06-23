5f3befe775c4ee92b909bf94cd4ae13345f6af87
uladzimir <01myit@gmail.com>
2025-06-23 21:10:05 +0300

    Pouch DB.

--- sv_form/src/routes/+page.svelte
+++ sv_form/src/routes/+page.svelte
@@ -1,2 +1,33 @@
+<script>
+// @ts-nocheck
+
+    import './styles.css'
+    import {todos, localTodosDB,  getAllTodos} from '$lib/client/pouchDB'
+
+    let reactTodos = $state(todos)
+    $inspect( todos )
+    async function deleteTodo(id) {
+	try {
+		const doc = await localTodosDB.get(id);
+		await localTodosDB.remove(doc);
+		// localTodosDB.compact();
+        reactTodos = await getAllTodos()
+	} catch (err) {
+		console.error('Remove ERROR:', err);
+	}
+}
+</script>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h3>Fill out the form below</h3>
+
+<form method="POST">
+    <label title="write todo">
+        <input type="text" placeholder="add a todo" name='todo'>
+    </label>
+</form>
+
+<ul>
+    {#each reactTodos as t (t)}
+        <li>{t.todo} <button onclick={() => deleteTodo(t._id)}>delete</button></li>
+    {/each}
+</ul>
--- sv_form/vite.config.ts
+++ sv_form/vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import devtools from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), devtools()],
+	define: {
+		global: 'window'
+	},
+	optimizeDeps: {
+		include: ['pouchdb', 'events']
+	}
 });

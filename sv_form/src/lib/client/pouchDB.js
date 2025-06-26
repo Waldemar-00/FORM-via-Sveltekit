b47374c906fa0e2141c35ab5c7e004cd2d3b9b85
uladzimir <01myit@gmail.com>
2025-06-26 19:09:09 +0300

    Checkbox was added and classes for it, and reactivity via PouchDB.

--- sv_form/src/lib/client/pouchDB.js
+++ sv_form/src/lib/client/pouchDB.js
@@ -30,51 +30,22 @@ export class LocalTodosDB {
 		}
 	}
 	async getAllTodos() {
-		const result = await this.#db.allDocs({
-			include_docs: true,
-			attachments: true
+		const docs = await this.#db.allDocs({
+			include_docs: true, // full document => default value is false => only _id & _rev
+			attachments: true // if docs have attachments like a photo => default false => without pages, files and act.
 		});
-		return await result.rows.map((row) => row.doc);
+		return await docs.rows.map((doc) => doc.doc);
+	}
+	async getTodoById(id) {
+		return await this.#db.get(id);
+	}
+	async checkboxChanged(e, t) {
+		t.done = e.target.checked;
+		this.#db.put(t);
 	}
 }
+// localTodosDB.compact(); no update CouchDB
 
-// const localTodosDB = new PouchDB('localTodos');
-
-// export async function addTodo(id: string, todo: string, done: boolean) {
-// 	await localTodosDB.put({
-// 		_id: id,
-// 		todo,
-// 		done,
-// 		type: 'todo'
-// 	});
-// 	return id;
-// }
-// addTodo(crypto.randomUUID(), 'Example', false);
-
-// export async function getTodoById(id: string) {
-// 	return await localTodosDB.get(id);
-// }
-// export async function getAllTodos() {
-// 	const result = await localTodosDB.allDocs({
-// 		include_docs: true,
-// 		attachments: true
-// 	});
-// 	return result.rows.map((row) => row.doc);
-// }
-
-// const todos = await getAllTodos();
-// export { todos, localTodosDB };
-
-// async function deleteTodo(id: string) {
-// 	try {
-// 		const doc = await localTodosDB.get(id);
-// 		await localTodosDB.remove(doc);
-// 		console.log(`Document with id=${id} removed`);
-// 		localTodosDB.compact();
-// 	} catch (err) {
-// 		console.error('Remove ERROR:', err);
-// 	}
-// }
 // async function bulkDelete(docs: any[]) {
 // 	// docs — массив документов с _id и _rev
 // 	const docsToDelete = docs.map((doc) => ({ ...doc, _deleted: true }));
@@ -83,15 +54,5 @@ export class LocalTodosDB {
 // 		console.log('Массовое удаление завершено', result);
 // 	} catch (err) {
 // 		console.error('Ошибка массового удаления:', err);
-// 	}
-// }
-// async function deleteTodo(id) {
-// 	try {
-// 		const doc = await localTodosDB.get(id);
-// 		await localTodosDB.remove(doc);
-// 		// localTodosDB.compact();
-// 		reactTodos = await getAllTodos();
-// 	} catch (err) {
-// 		console.error('Remove ERROR:', err);
 // 	}
 // }
--- sv_form/src/routes/+page.svelte
+++ sv_form/src/routes/+page.svelte
@@ -6,6 +6,7 @@
 
     let reactTodos = $state([])
     let value = $state('')
+
     const localTodosDB = new PouchDB('localTodos')
     const DB = new LocalTodosDB(localTodosDB)
     DB.getAllTodos().then(todos => {reactTodos = todos})
@@ -15,8 +16,9 @@
 
 <form>
     <label title="write todo">
-        <input type="text" placeholder="add a todo" name='todo' bind:value onchange={async () => {
-            await DB.addTodo(crypto.randomUUID(), value, false)
+        <!-- svelte-ignore a11y_autofocus -->
+        <input type="text" placeholder="add a todo" name='todo' autofocus bind:value onchange={async () => {
+            await DB.addTodo(new Date().toISOString(), value, false)
             DB.getAllTodos().then(todos => {reactTodos = todos})
         }}>
     </label>
@@ -24,11 +26,52 @@
 
 <ul>
     {#each reactTodos as t (t._id)}
-        <li>{t.todo} <button onclick={async () => {
-            await DB.deleteTodo(t._id)
-            DB.getAllTodos().then(todos => {
-                reactTodos = todos
-            })
-        }}>delete</button></li>
+
+        <li>
+            <input type="checkbox" checked={t.done} onchange={(e) => { DB.checkboxChanged(e, t) }}>
+            <div>
+                <span>{t.todo}</span>
+                <button onclick={async () => {
+                    await DB.deleteTodo(t._id)
+                    DB.getAllTodos().then(todos => {
+                        reactTodos = todos
+                    })
+                }}>delete</button>
+            </div>
+        </li>
     {/each}
 </ul>
+
+<style>
+    button {
+        background-color: #fff;
+        border: none;
+        font-size: 0.7rem;
+        font-style: italic;
+        color: red;
+    }
+    li {
+        display: flex;
+        gap: 0.7rem;
+    }
+    li div {
+        min-width: 30vw;
+        display: flex;
+        justify-content: space-between;
+        gap: 1rem;
+        padding: 0.5rem 1.5rem;
+        border: 1px solid #eee;
+        border-radius: 3px;
+        margin-bottom: 0.5rem;
+    }
+    [name='todo'] {
+        padding: 0.5rem 1.5rem;
+        border-radius: 3px;
+        border: 1px solid #eee;
+        margin-bottom: 0.5rem;
+        outline: none;
+    }
+    [name='todo']:focus {
+        outline: 2px solid orange
+    }
+</style>

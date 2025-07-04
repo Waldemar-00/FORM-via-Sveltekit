b176e1558aabd144fa5c8b783e9ae05d6b8a78b4
uladzimir <01myit@gmail.com>
2025-07-04 12:58:31 +0300

    Fail in try-catch.

--- sv_form/src/routes/sv_actions/+page.server.ts
+++ sv_form/src/routes/sv_actions/+page.server.ts
@@ -1,4 +1,5 @@
 import PouchDB from 'pouchdb';
+import { fail } from '@sveltejs/kit';
 import { LocalTodosDB } from '$lib/client/pouchDB';
 import { ADMIN, PASSWORD } from '$env/static/private';
 const serverLocalTodosDB = new PouchDB('Server_PouchDB_todos');
@@ -18,18 +19,52 @@ export const actions = {
 	addTodo: async ({ request }) => {
 		const todo = await request.formData();
 		const todoStr = todo.get('todo');
-		await DB.addTodo(new Date().toISOString(), todoStr, false);
+		try {
+			// throw new Error('Handle ERROR!!!!!');
+			await DB.addTodo(new Date().toISOString(), todoStr, false);
+		} catch (error) {
+			let message;
+			if (error instanceof Error) message = error.message;
+			return fail(500, {
+				description: todoStr,
+				error: message
+			});
+		}
 	},
 	deleteTodo: async ({ request }) => {
 		const todo = await request.formData();
 		const id = todo.get('id');
-		await DB.deleteTodo(id);
+		try {
+			// throw new Error('Has not been deleted todo!!!');
+			await DB.deleteTodo(id);
+		} catch (error) {
+			if (error instanceof Error) {
+				return fail(500, {
+					description: `Todo id is ${id}`,
+					error: error.message
+				});
+			}
+		}
 	},
 	toggleCheckboxOnServer: async ({ request }) => {
 		const checkbox = await request.formData();
 		const checked = checkbox.get('toggleCheckbox');
 		const id = checkbox.get('id');
-		if (checked) await DB.toggleCheckboxOnServer(true, id);
-		else await DB.toggleCheckboxOnServer(false, id);
+		try {
+			throw id;
+			if (checked) await DB.toggleCheckboxOnServer(true, id);
+			else await DB.toggleCheckboxOnServer(false, id);
+		} catch (error) {
+			if (error instanceof Error) {
+				return fail(500, {
+					description: `Todo id is ${id}!!!`,
+					error: error.message
+				});
+			} else {
+				return fail(500, {
+					error: `Has not been toggle a todo\nwith id: ${error}!!!`
+				});
+			}
+		}
 	}
 };
--- sv_form/src/routes/sv_actions/+page.svelte
+++ sv_form/src/routes/sv_actions/+page.svelte
@@ -1,11 +1,16 @@
 <script>
      import '../styles.css'
      import {enhance} from '$app/forms'
-     let {data} = $props()
+     let {data, form } = $props()
 </script>
 <h2>Actions</h2>
 
-
+{#if form?.error}
+<div class=error>
+    <h2><pre>{form?.description}</pre></h2>
+    <h2><pre>{form?.error}</pre></h2>
+</div>
+{/if}
 <form method="POST" action='?/addTodo'>
     <label title="write todo">
         <!-- svelte-ignore a11y_autofocus -->
@@ -36,6 +41,9 @@
     {/each}
 </ul>
 <style>
+    div h2 {
+        color: red
+    }
     li {
         display: flex;
         gap: 0.7rem;
@@ -49,5 +57,10 @@
         border: 1px solid #eee;
         border-radius: 3px;
         margin-bottom: 0.5rem;
+    }
+    .error {
+        position:fixed;
+        right: 0;
+        top: 0;
     }
 </style>
